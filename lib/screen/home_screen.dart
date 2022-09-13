64321eb2ec20d7a83378c27472f617f308498df3
seongwon <syanggu@naver.com>
2022-09-13 21:29:49 +0900

    color 수정, map 코드 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,40 +12,71 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primary_color,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('랜덤숫자 생성기'),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.settings,
-                  ),
-                )
-              ],
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('123'),
-                  Text('456'),
-                  Text('789'),
+                  Text(
+                    '랜덤숫자 생성기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.settings,
+                      color: Colors.red,
+                    ),
+                  )
                 ],
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('생성하기'),
+              Expanded(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      123,
+                      456,
+                      789,
+                    ]
+                        .map(
+                          (x) => Row(
+                            children: x
+                                .toString()
+                                .split('')
+                                .map(
+                                  (y) => Image.asset(
+                                    'asset/img/$y.png',
+                                    height: 70.0,
+                                    width: 50.0,
+                                  ),
+                                )
+                                .toList(),
+                          ),
+                        )
+                        .toList()),
               ),
-            ),
-          ],
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: red_color,
+                  ),
+                  onPressed: () {},
+                  child: Text('생성하기'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
